547d455f1031b2aa6bbb358dd1a143b37af98938
Raiyan ALLADUTT <raiyan.alladutt@gmail.com>
2026-01-14 15:35:32 +0100

    mini projet v2

--- mini_projet/auth-service/app.py
+++ mini_projet/auth-service/app.py
@@ -4,19 +4,19 @@ import datetime
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) # Pour que le HTML puisse l'appeler
+CORS(app)
 
-# Clé secrète pour signer les badges (Tokens)
+
 SECRET_KEY = "super_secret_key_microservices"
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
     
-    # Vérification simple (admin / admin)
+
     if data and data.get('username') == "admin" and data.get('password') == "admin":
         
-        # Création du token
+
         token = jwt.encode({
             'user': 'admin',
             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
@@ -27,5 +27,4 @@ def login():
     return jsonify({'message': 'Mauvais login/mot de passe !'}), 401
 
 if __name__ == '__main__':
-    # Port 5003 pour ce service
     app.run(host='0.0.0.0', port=5003, debug=True)
